323970dfabc5e499744b0591e2277d51332cb2ee
dima brodski <dima001@gmail.com>
2018-03-24 15:38:27 +0300

    ToDo-app is worksing!

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { Todo } from '../todo';
+import {TodoDataService} from '../todo-data.service';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+
+  @Input()
+  todos: Todo[];
+
+  @Output()
+  remove: EventEmitter<Todo> = new EventEmitter();
+
+  @Output()
+  toggleComplete: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoDataService: TodoDataService) { }
+
+
+  onToggleTodoComplete(todo: Todo) {
+    this.toggleComplete.emit(todo);
+  }
+
+  onRemoveTodo(todo: Todo) {
+    this.remove.emit(todo);
+  }
+  
+
+}
